25a987bf7bb470a7f0055b607f81ae1b7b2624cb
Logan Reed <logan.c.reed1@gmail.com>
2025-02-08 00:26:37 -0500

    Remove experimental.appDir from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false,  // Disable the new app directory feature
-  },
 };
 
 export default nextConfig;
+
 
