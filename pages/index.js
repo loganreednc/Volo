f4f0e4afbf16483dd890b0fe02ad2a2624964143
Logan Reed <logan.c.reed1@gmail.com>
2025-02-07 23:14:29 -0500

    Ensure minimal index.js for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
-// Redeploy test: updated homepage
 // pages/index.js
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div className="text-center p-8 bg-gray-100 min-h-screen">
-      <h1 className="text-4xl font-bold mb-4 text-blue-900">Welcome to CatholicMatch</h1>
-      <p className="text-lg mb-6">Find your match, curated with care.</p>
-      <div>
-        <Link href="/signup" className="mr-4 text-blue-500 hover:underline">
-          Sign Up
-        </Link>
-        <Link href="/login" className="text-blue-500 hover:underline">
-          Log In
-        </Link>
-      </div>
+    <div>
+      <h1>Welcome to CatholicMatch</h1>
+      <p>This is the homepage.</p>
     </div>
   );
 }
+
 
