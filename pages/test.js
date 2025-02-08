bc6630a0a41a9e076e1a0a6192bdcb97624b229d
Logan Reed <logan.c.reed1@gmail.com>
2025-02-08 14:31:55 -0500

    Update /test page to force dynamic rendering and be a minimal test page

--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,13 @@
 // pages/test.js
+export const dynamic = 'force-dynamic';
+
 export default function TestPage() {
-    return <div>Test Page Loaded Successfully!</div>;
-  }
+  return (
+    <div className="p-4">
+      <h1>Test Page</h1>
+      <p>This page is for testing purposes and is rendered dynamically.</p>
+    </div>
+  );
+}
+
   
