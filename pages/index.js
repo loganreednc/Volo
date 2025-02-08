ee0a72c5c87ae7d68a08f53c25d16a654c975991
Logan Reed <logan.c.reed1@gmail.com>
2025-02-08 14:18:23 -0500

    Force dynamic rendering on homepage to avoid pre-render Mongoose metadata errors

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 // pages/index.js
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Volo</h1>
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Welcome to Volo</h1>
       <p>This is the homepage.</p>
     </div>
   );
 }
+
 
 
 
